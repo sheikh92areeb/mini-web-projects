65be01b85f4088c07f6053bfaff2f734ac5b7828
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-09-13 18:47:09 +0500

    working

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+console.log("let's play with scripting in JS");
+
+// function toggleDropdown(button) {
+//     const dropdown = button.nextElementSibling;
+//     const caretRight = button.querySelector(".fa-caret-right");
+//     const caretDown = button.querySelector(".fa-caret-down");
+
+//     console.log(caretDown, caretRight);
+
+//     dropdown.classList.toggle("hidden");
+//     caretRight.classList.toggle("hidden");
+//     caretDown.classList.toggle("hidden");
+// }
+
+document.querySelectorAll('.dropdown-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+      const dropdown = button.nextElementSibling; // the div.dropdown
+      if (!dropdown) return;
+
+      dropdown.classList.toggle('hidden');
+
+      // update aria-expanded
+      const expanded = !dropdown.classList.contains('hidden');
+      button.setAttribute('aria-expanded', expanded);
+
+      // if there are caret icons, toggle them too (optional)
+      const caretRight = button.querySelector('.fa-caret-right');
+      const caretDown  = button.querySelector('.fa-caret-down');
+      if (caretRight && caretDown) {
+        caretRight.classList.toggle('hidden', expanded);
+        caretDown.classList.toggle('hidden', !expanded);
+      }
+    });
+  });
